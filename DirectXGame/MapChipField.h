6237a3065ff67c29d2897890e8386fee4b2fb2f2
youta-mitomo <k023g1024@g.neec.ac.jp>
2024-08-09 16:35:25 +0900

    kari

--- DirectXGame/MapChipField.h
+++ DirectXGame/MapChipField.h
@@ -30,8 +30,8 @@ public:
 private:
 
 	// 1ブロックのサイズ
-	static inline const float kBlockWidth = 2.0f;
-	static inline const float kBlockHeight = 2.0f;
+	static inline const float kBlockWidth = 1.0f;
+	static inline const float kBlockHeight = 1.0f;
 	// ブロックの個数
 	static inline const uint32_t kNumBlockVirtical = 20;
 	static inline const uint32_t kNumBlockHorizontal = 100;
